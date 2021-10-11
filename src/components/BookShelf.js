584eba0adc7202f366af942ee845efa0dd4fa52e
mamume <mmustafametwally@gmail.com>
2021-10-12 00:15:28 +0200

    Adding some styles

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Book from './Book'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function BookShelf(props) {
     return (
         <div className="bookshelf">
-            <h2 className="bookshelf-title">{props.title}</h2>
+            <h2 className="bookshelf-title d-flex p-3">
+                <div className='mx-auto'>{props.title}</div>
+            </h2>
             <div className="bookshelf-books">
                 <ol className="books-grid">
 
